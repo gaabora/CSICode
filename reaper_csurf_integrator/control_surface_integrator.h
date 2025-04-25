fb23c8c67ee6511b6db598936219ead3fc6f4098
gaabora <gaabora@gmail.com>
2025-04-25 10:52:14 +0300

    feat: add option RunCount to avoid line duplication if need to repeat several action n>1 times

--- reaper_csurf_integrator/control_surface_integrator.h
+++ reaper_csurf_integrator/control_surface_integrator.h
@@ -143,6 +143,7 @@ enum PropertyType {
   D(Feedback) \
   D(HoldDelay) \
   D(HoldRepeatInterval) \
+  D(RunCount) \
   D(OSD) \
   D(Version) \
   D(SurfaceType) \
@@ -508,6 +509,8 @@ private:
     bool holdActive_= false;
     bool holdRepeatActive_ = false;
     double deferredValue_ = 0.0;
+    
+    int  runCount_ = 1;
     
     bool supportsColor_ = false;
     vector<rgba_color> colorValues_;
