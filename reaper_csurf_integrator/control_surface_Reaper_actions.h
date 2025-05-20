smarty56/CSICode-V1M

Added TrackRecordArmDisplay action

reaper_csurf_integrator/control_surface_Reaper_actions.h
@@ -1123,6 +1123,29 @@ public:
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class TrackRecordArmDisplay : public Action
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+{
+public:
+    virtual const char* GetName() override { return "TrackRecordArmDisplay"; }
+
+    virtual void RequestUpdate(ActionContext* context) override
+    {
+        if (MediaTrack* track = context->GetTrack())
+        {
+            double state = GetMediaTrackInfo_Value(track, "I_RECARM");
+
+            if (state > 0.5)
+                context->UpdateWidgetValue("ARM");
+            else
+                context->UpdateWidgetValue("");
+        }
+        else
+            context->ClearWidget();
+    }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class TrackMute : public Action
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 {

gaabora/CSICode

reaper_csurf_integrator/control_surface_Reaper_actions.h
@@ -846,6 +846,29 @@ public:
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class TrackRecordArmDisplay : public DisplayAction
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+{
+public:
+    virtual const char* GetName() override { return "TrackRecordArmDisplay"; }
+
+    virtual void RequestUpdate(ActionContext* context) override
+    {
+        if (MediaTrack* track = context->GetTrack())
+        {
+            double state = GetMediaTrackInfo_Value(track, "I_RECARM");
+
+            if (state > 0.5)
+                context->UpdateWidgetValue("ARM");
+            else
+                context->UpdateWidgetValue("");
+        }
+        else
+            context->ClearWidget();
+    }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class TrackMute : public Action
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 {